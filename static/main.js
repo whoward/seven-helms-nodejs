3cd70e403ab2f1c397d14cd0aeda9ee4eddee73a
William Howard <whoward.tke@gmail.com>
2011-07-28 04:00:27 -0400

    much better input handler, probably wont work for mobile, but this is ok for now

--- static/main.js
+++ static/main.js
@@ -22,67 +22,23 @@ $(document).ready(function() {
       switch(code) {
          case 13: // enter key
             webSocket.emit("message", input);
-            input = "";            
+            input = "";
             break;
 
          case 8: // backspace key
             input = input.slice(0, input.length - 1);
             break;
-
-         case 112: // Function Keys F1->F12
-         case 113:
-         case 114:
-         case 115:
-         case 116:
-         case 117:
-         case 118:
-         case 119:
-         case 120:
-         case 121:
-         case 122:
-         case 123:
-            return true;
-
-         case 188: // comma key
-            input += ",";
          
-         case 189: // dash key
-            input += "-";
-
-         case 190: // period key
-            input += ".";
-
-         case 191: // forward slash key
-            input += "/";
-
-         case 219: // open bracket key
-            input += "(";
-
-         case 220: // backslash key
-            input += "\\";
-
-         case 221: // close bracket key
-            input += ")";
-
-         case 222: // single quote key
-            input += "'";
-
-
-
-         default: // any valid unhandled keyboard key
-            // if in the string character range and the shift key is not pressed, then downcase the letter
-            if(65 <= code && code <= 90 && !e.shiftKey) {
-               code = code + 32;
-            }
-
-            // and append it to the input
-            input += String.fromCharCode(code);
+         default:
+            return true;
       }
 
       $("#console").html("> " + input + "_");
-
-      // consider also: tab=9, shift=16, ctrl=17, alt=18, pause/break=19 capslock=
-      // http://www.cambiaresearch.com/c4/702b8cd1-e5b0-42e6-83ac-25f0306e3e25/javascript-char-codes-key-codes.aspx
       return false;
+   });
+
+   $(document).bind("keypress", function(e) {
+      input += String.fromCharCode(e.charCode);
+      $("#console").html("> " + input + "_");
    });
 });
