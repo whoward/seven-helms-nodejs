6c169a95bdddf4cd2e95cddcf75d8e51251afd1e
William Howard <whoward.tke@gmail.com>
2011-07-31 22:52:42 -0400

    update boot production script

--- boot-production.js
+++ boot-production.js
@@ -4,9 +4,13 @@ var path = require("path");
 
 // assign the environment if undefined
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
+process.env.PORT = process.env.PORT || 80;
+
+console.log("node environment:", process.env.NODE_ENV);
+console.log("node port:", process.env.PORT);
 
 // get the path to the server coffeescript file
-server_file = path.join(__dirname, "server.coffee")
+server_file = path.join(__dirname, "server.coffee");
 
 // read and evaluate the server coffeescript
 fs.readFile(server_file, function(err, data) {
